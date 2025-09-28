06f2743e27f213edb48f83609d6583430b30c493
Breuß Valentin <valentin.breuss@baur.eu>
2025-09-28 21:43:21 +0200

    Fix typo

--- Source/Mockerade/Checks/CheckResult.cs
+++ Source/Mockerade/Checks/CheckResult.cs
@@ -17,37 +17,37 @@ public class CheckResult : ICheckResult
 	Invocation[] ICheckResult.Invocations => _invocations;
 
 	/// <summary>
-	/// … at least the expected number of <paramref name="times" />.
+	/// …at least the expected number of <paramref name="times" />.
 	/// </summary>
 	public bool AtLeast(int times) => _invocations.Length >= times;
 
 	/// <summary>
-	/// … at least once.
+	/// …at least once.
 	/// </summary>
 	public bool AtLeastOnce() => _invocations.Length >= 1;
 
 	/// <summary>
-	/// … at most the expected number of <paramref name="times" />.
+	/// …at most the expected number of <paramref name="times" />.
 	/// </summary>
 	public bool AtMost(int times) => _invocations.Length <= times;
 
 	/// <summary>
-	/// … at most once.
+	/// …at most once.
 	/// </summary>
 	public bool AtMostOnce() => _invocations.Length <= 1;
 
 	/// <summary>
-	/// … exactly the expected number of <paramref name="times" />.
+	/// …exactly the expected number of <paramref name="times" />.
 	/// </summary>
 	public bool Exactly(int times) => _invocations.Length == times;
 
 	/// <summary>
-	/// … exatly once.
+	/// …exactly once.
 	/// </summary>
 	public bool Once() => _invocations.Length == 1;
 
 	/// <summary>
-	/// … never.
+	/// …never.
 	/// </summary>
 	public bool Never() => _invocations.Length == 0;
 
